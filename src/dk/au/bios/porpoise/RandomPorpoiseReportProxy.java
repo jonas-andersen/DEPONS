e5a059ab5e453b8f6958bcc83ad82c4dd2616afd
Jonas Andersen <ja@aragost.com>
2025-05-14 21:49:27 +0200

    separate turbine and ship deterrence

--- src/dk/au/bios/porpoise/RandomPorpoiseReportProxy.java
+++ src/dk/au/bios/porpoise/RandomPorpoiseReportProxy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2023 Jacob Nabe-Nielsen <jnn@bios.au.dk>
+ * Copyright (C) 2017-2025 Jacob Nabe-Nielsen <jnn@bios.au.dk>
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
  * License version 2 and only version 2 as published by the Free Software Foundation.
@@ -82,9 +82,17 @@ public class RandomPorpoiseReportProxy extends Agent {
 		}
 	}
 
-	public double getDeterStrength() {
+	public double getDeterTurbineStrength() {
 		if (porpoise.isAlive()) {
-			return convertGridDistanceToUtm(porpoise.getDeterStrength());
+			return convertGridDistanceToUtm(porpoise.getDeterTurbineStrength());
+		} else {
+			return -1;
+		}
+	}
+
+	public double getDeterShipStrength() {
+		if (porpoise.isAlive()) {
+			return convertGridDistanceToUtm(porpoise.getDeterShipStrength());
 		} else {
 			return -1;
 		}
